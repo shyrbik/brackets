1f701ce7a145af1abfb019ef02ca7938dc9ae5ac
shyrbik <58969166+shyrbik@users.noreply.github.com>
2021-02-26 12:34:38 +0300

    feat: add solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    
+    let confArr = bracketsConfig.map((i) => i[0] + i[1]);
+
+    let stringForCompare = str;
+    while (true) {
+        for (let i = 0; i < confArr.length; i++) {
+            str = str.replace(confArr[i], '');
+        };
+
+        if (str.length == 0) {
+            return true;
+        };
+        if (stringForCompare === str) {
+            return false;
+        };
+
+        stringForCompare = str;
+    };
+};
